ce6fa76e1273282780a42506d23277bfef9aa75f
Angad Sethi <angad.sethi@atlan.com>
2024-07-31 15:15:31 +0530

    feat: add support for Vue

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-export { Choice, JsonSchemaViewer, useChoices, Validations } from './components';
+import { Choice, JsonSchemaProps, JsonSchemaViewer, useChoices, Validations } from './components';
+import { createHydrate, createRender } from './standalone-codebase';
+
 export { visibleChildren } from './tree';
 export * from './types';
+export { Choice, JsonSchemaProps, JsonSchemaViewer, useChoices, Validations };
+export default {
+  render: createRender<JsonSchemaProps>(JsonSchemaViewer),
+  hydrate: createHydrate<JsonSchemaProps>(JsonSchemaViewer),
+};
---
+++ src/standalone-codebase.ts
@@ -0,0 +1,49 @@
+import React from 'react';
+import { createRoot, hydrateRoot } from 'react-dom/client';
+
+function querySelector(selector: string): Element | null {
+  if (typeof document !== 'undefined') {
+    return document.querySelector(selector);
+  }
+  return null;
+}
+
+/**
+ * A factory that creates the rendering function of the given React component of any kind.
+ *
+ * @param {Any} component of any kind
+ */
+export function createRender<Props extends Parameters<typeof React.createElement>[1]>(
+  component: Parameters<typeof React.createElement>[0],
+) {
+  return (props: Props, container?: Element | DocumentFragment | null) => {
+    container = container ?? querySelector('asyncapi');
+
+    if (container === null) {
+      return;
+    }
+
+    const root = createRoot(container);
+
+    root.render(React.createElement(component, props));
+  };
+}
+
+/**
+ * A factory that creates the hydration function of the given React component of any kind.
+ *
+ * @param {Any} component of any kind
+ */
+export function createHydrate<Props extends Parameters<typeof React.createElement>[1]>(
+  component: Parameters<typeof React.createElement>[0],
+) {
+  return (props: Props, container?: Element | Document | null) => {
+    container = container ?? querySelector('asyncapi');
+
+    if (container === null) {
+      return;
+    }
+
+    hydrateRoot(container, React.createElement(component, props));
+  };
+}
